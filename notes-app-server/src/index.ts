6816ca67d8ec42e9961a17ca2a4ccbeaa83d64b2
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2025-01-03 16:07:24 -0500

    fixed server

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -21,14 +21,57 @@ app.post("/api/notes", async (req, res) => {
     const { title, content } = req.body;
 
     if (!title || !content) {
-        return res.status(400).send("title and content fields required");
+        res.status(400).send("title and content fields required");
+        return
     }
-
     try {
         const note = await prisma.note.create({
             data: { title, content },
         });
         res.json(note);
+    } catch (error) {
+        res.status(500).send("Oops, something went wrong");
+    }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+    const { title, content } = req.body;
+    const id = parseInt(req.params.id);
+
+    if (!title || !content) {
+        res.status(400).send("title and content fields required");
+        return
+    }
+
+    if (!id || isNaN(id)) {
+        res.status(400).send("ID must be a valid number");
+        return
+    }
+
+    try {
+        const updatedNote = await prisma.note.update({
+            where: { id },
+            data: { title, content },
+        });
+        res.json(updatedNote);
+    } catch (error) {
+        res.status(500).send("Oops, something went wrong");
+    }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (!id || isNaN(id)) {
+        res.status(400).send("ID field required");
+        return
+    }
+
+    try {
+        await prisma.note.delete({
+            where: { id },
+        });
+        res.status(204).send();
     } catch (error) {
         res.status(500).send("Oops, something went wrong");
     }
